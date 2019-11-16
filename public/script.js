5eb55cda3f333ca6a6382d22691b8e3d5d702019
tzvik15 <tzvik15@yahoo.com>
2019-11-16 11:11:27 -0700

    changed folder structure and paths accordingly

--- public/script.js
+++ public/script.js
@@ -6,10 +6,12 @@ $("#submit").on("click", function(event) {
       email: $("#email").val().trim(),
       ID: $("#id").val().trim()
     };
-
+    console.log(newRes)
     // Question: What does this code do??
     $.post("/api/tables", newRes)
       .then(function(data) {
         alert("Thank you for your desire to visit our Dragon restauraunt. Please wait an indefinate time in cyberspace and maybe we will serve you. yay.");
       });
-  });
+   });
+
+   //module.exports = script;
